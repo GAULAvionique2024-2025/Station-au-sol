d976b5cd016df8035d6cc8ceb7e55b5b8fe4b18c
Mathouqc <32655672+mathouqc@users.noreply.github.com>
2023-10-02 03:11:04 -0400

    New test-server
    Nouveau test-server avec un script qui permet de générer des logs selon une fonction (voir lien desmos)
    https://www.desmos.com/calculator/8iy3mnkjzw?lang=fr

---
+++ test-server/create_log.py
@@ -0,0 +1,55 @@
+# Desmos: https://www.desmos.com/calculator/8iy3mnkjzw?lang=fr
+import math
+import random
+
+
+def main():
+    with open("new_log.txt", "w") as file:
+        file.write("time,alt,pitch,roll,yaw,lat,lon\n")
+        for i in range(60):
+            time = i
+            alt = alt_function(i)
+            pitch = pitch_function(i)
+            roll = roll_function(i, alt)
+            lat = lat_function(i, 46.789488, 46.789694, 60)
+            lon = lon_function(i, -71.477804, -71.472765, 60)
+            file.write(f"{time},{alt},{pitch},{roll},0,{lat},{lon}\n")
+
+
+# 10000-20\left(x-25\right)^{2}
+def alt_function(x):
+    y = 10000 - 20 * (x - 25) ** 2
+    return y if y > 0 else 0
+
+
+# Derivative of alt_function: -40\left(x-25\right)
+def pitch_function(x):
+    a = -40 * (x - 25)
+    pitch = round(math.atan(a/100) * 180 / math.pi, 1)
+    return pitch
+
+
+# \left(\frac{x-25}{3}\right)^{3}+500
+def roll_function(x, alt):
+    roll = ((x - 25)/3) ** 3 + 500
+    roll += random.randint(-10, 10)
+    roll = round(roll, 1)
+    return roll if alt > 0 else 0
+
+
+def lat_function(x, lat_min, lat_max, count):
+    step = (lat_max - lat_min) / count
+    lat = lat_min + step * x
+    lat += random.randint(-2, 2) * 0.0001
+    return round(lat, 6)
+
+
+def lon_function(x, lon_min, lon_max, count):
+    step = (lon_max - lon_min) / count
+    lon = lon_min + step * x
+    lon += random.randint(-2, 2) * 0.0001
+    return round(lon, 6)
+
+
+if __name__ == "__main__":
+    main()
--- test-server/main.py
+++ test-server/main.py
@@ -1,34 +1,70 @@
 import asyncio
 import websockets
 import csv
+import time
+import json
 
 
-async def new_data():
-    # Read logs
-    with open("log.txt", "r") as file:
-        reader = list(csv.reader(file))
-        # Remove Header
-        reader.pop(0)
-        # Send data each second
-        for row in reader:
-            await asyncio.sleep(1)
-            return {
-                "time": row[0],
-                "alt": row[1],
-                "pitch": row[2],
-                "roll": row[3],
-                "yaw": row[4],
-            }
+class Data:
+    def __init__(self):
+        self.datalist = self.read_datalist()
+        self.i = 0
+        self.max_i = len(self.datalist) - 1
+        self.lasttime = time.time()
 
+    @staticmethod
+    def read_datalist():
+        datalist = []
+        with open("log.txt", "r") as file:
+            reader = list(csv.reader(file))
+            # Remove Header
+            reader.pop(0)
+            # Create data
+            for row in reader:
+                datalist.append({
+                    "time": row[0],
+                    "alt": row[1],
+                    "pitch": row[2],
+                    "roll": row[3],
+                    "yaw": row[4],
+                    "lat": row[5],
+                    "lon": row[6]
+                })
+            return datalist
 
+    def get_new_data(self):
+        if self.i <= self.max_i:
+            data = self.datalist[self.i]
+        else:
+            self.i = 0
+            data = self.datalist[self.i]
+        self.i += 1
+        return data
+
+    async def fetch_new_data(self):
+        # Check if timenow > lasttime + 1
+        if time.time() > self.lasttime + 1:
+            self.lasttime = time.time()
+            return self.get_new_data()
+        else:
+            await asyncio.sleep(self.lasttime + 1 - time.time())
+            self.lasttime = time.time()
+            return self.get_new_data()
 
 
 # create handler for each connection (send data to client)
 async def handler(websocket):
+    new_data = Data()
     async for message in websocket:
+        print("Received: " + message)
         if message == "OK":
-            data = await new_data()
-            await websocket.send(data)
+            data = await new_data.fetch_new_data()
+            try:
+                await websocket.send(json.dumps(data))
+            except websockets.exceptions.ConnectionClosed:
+                print("Connection closed")
+                break
+            print("Sent: " + str(data))
 
 
 # create server
