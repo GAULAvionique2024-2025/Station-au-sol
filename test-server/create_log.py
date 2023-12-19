222dded8ef4efe5518b31eee3f9b23adff480256
Mathouqc <32655672+mathouqc@users.noreply.github.com>
2023-12-19 10:56:24 -0500

    Update create_log.py
    Meilleurs commentaires et fonctions de vitesse et d'accélération

--- test-server/create_log.py
+++ test-server/create_log.py
@@ -1,28 +1,41 @@
-# Desmos: https://www.desmos.com/calculator/8iy3mnkjzw?lang=fr
+# Desmos: https://www.desmos.com/calculator/8iy3mnkjzw
 import math
 import random
 
 
+# Main function to create a log file
+# Each column is generated with a function, which take at least the time as parameter
 def main():
     with open("new_log.txt", "w") as file:
-        file.write("time,alt,pitch,roll,yaw,lat,lon\n")
+        file.write(
+            "time,altitude,pitch,roll,yaw,lat,lon,speed,acceleration,vibrations,landing_force,temperature,batt_check,igniter_check,gps_check\n"
+        )
+        # Peak at 25 seconds
         for i in range(60):
             time = i
-            alt = alt_function(i)
+            altitude = altitude_function(i)
             pitch = pitch_function(i)
-            roll = roll_function(i, alt)
+            roll = roll_function(i, altitude)
             lat = lat_function(i, 46.789488, 46.789694, 60)
             lon = lon_function(i, -71.477467, -71.472164, 60)
-            file.write(f"{time},{alt},{pitch},{roll},0,{lat},{lon}\n")
+            speed = speed_function(i, altitude)
+            acceleration = acceleration_function(i, altitude)
+            file.write(
+                f"{time},{altitude},{pitch},{roll},0,{lat},{lon},{speed},{acceleration}\n"
+            )
 
 
+# Parabolic function: y = 10000 - 20(x - 25)^2
+# (Peak at 25)
 # 10000-20\left(x-25\right)^{2}
-def alt_function(x):
+def altitude_function(x):
     y = 10000 - 20 * (x - 25) ** 2
+    # 0 if negative
     return y if y > 0 else 0
 
 
-# Derivative of alt_function: -40\left(x-25\right)
+# Angle from derivative of altitude_function: y = -40(x - 25)
+# -40\left(x-25\right)
 def pitch_function(x):
     a = -40 * (x - 25)
     pitch = round(math.atan(a/100) * 180 / math.pi, 1)
@@ -30,13 +43,15 @@ def pitch_function(x):
 
 
 # \left(\frac{x-25}{3}\right)^{3}+500
-def roll_function(x, alt):
+def roll_function(x, altitude):
     roll = ((x - 25)/3) ** 3 + 500
     roll += random.randint(-10, 10)
     roll = round(roll, 1)
-    return roll if alt > 0 else 0
+    # No roll if on the ground (altitude = 0)
+    return roll if altitude > 0 else 0
 
 
+# From lat_min to lat_max with random noise
 def lat_function(x, lat_min, lat_max, count):
     step = (lat_max - lat_min) / count
     lat = lat_min + step * x
@@ -44,11 +59,28 @@ def lat_function(x, lat_min, lat_max, count):
     return round(lat, 6)
 
 
+# From lon_min to lon_max with random noise
 def lon_function(x, lon_min, lon_max, count):
     step = (lon_max - lon_min) / count
     lon = lon_min + step * x
     lon += random.randint(-2, 2) * 0.0001
     return round(lon, 6)
+
+
+# Derivative of altitude_function: y = -40(x - 25)
+# -40\left(x-25\right)
+def speed_function(x, altitude):
+    speed = -40 * (x - 25)
+    speed += random.randint(-2, 2)
+    return speed if altitude > 0 else 0
+
+
+# Derivative of speed_function: y = -40 + 1000
+# -40\left(x-25\right)
+def acceleration_function(x, altitude):
+    acceleration = -40
+    acceleration += random.randint(-2, 2)
+    return acceleration if altitude > 0 else 0
 
 
 if __name__ == "__main__":
