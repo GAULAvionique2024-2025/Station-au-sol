4e0a83b3d43bb2d92b95c419b5d3cb35e9efc609
Mathouqc <32655672+mathouqc@users.noreply.github.com>
2023-12-20 11:05:30 -0500

    Update server.js
    Ajout commentaire

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = 80
 
+// Fourni les fichiers statiques du dossier client
 app.use(express.static(path.join(__dirname, '..', 'client')));
 
 app.listen(port, () => {
