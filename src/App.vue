18a1fa301d944c595a161f9b53fcda55904e3eb0
Mathouqc <32655672+mathouqc@users.noreply.github.com>
2024-03-05 00:35:49 -0500

    Ajout scss et header

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import MyHeader from './components/MyHeader.vue';
 </script>
 
 <template>
-  <div>
+  <header>
+    <MyHeader></MyHeader>
+  </header>
+  <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  </div> -->
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
 <style scoped>
-.logo {
+/* .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
@@ -26,5 +30,5 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+} */
+</style>./components/MyHeader.vue
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,55 @@
+<script setup>
+</script>
+
+<template>
+    <!-- Main Header -->
+    <div id="top-header" class="d-flex justify-content-between">
+      <!-- Logo + Title -->
+      <div class="d-flex align-items-center">
+        <img id="logo-gaul" src="../assets/img/logo-gaul.svg" alt="Logo GAUL" width="100" height="24" />
+        <h1 class="text-white d-sm">Ground station</h1>
+      </div>
+
+      <!-- Options -->
+      <nav>
+        <div class="d-flex align-items-center h-100">
+          <!-- Buttons -->
+          <div id="header-options" class="d-md align-items-center h-100">
+            <a class="btn btn-link" href="./simple.html">Simple</a>
+            <button class="btn btn-secondary" data-btn="reset">
+              Reset
+            </button>
+            <button class="btn btn-danger btn-pause" data-btn="pause">
+              Pause
+            </button>
+            <button class="btn btn-icon">
+              <img src="../assets/img/settings.svg" alt="Settings Icon" height="36" width="36" data-btn="settings" />
+            </button>
+          </div>
+
+          <!-- Small screen btn -->
+          <button class="h-md btn btn-icon mx-1" data-btn="expand">
+            <img id="expand-ico" src="../assets/img/expand.svg" alt="Expand Icon" height="36" width="36" />
+          </button>
+        </div>
+      </nav>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.d-flex {
+  display: flex !important;
+}
+
+.justify-content-between {
+  justify-content: space-between;
+}
+
+.align-items-center {
+  align-items: center;
+}
+
+.h-100 {
+  height: 100%;
+}
+</style>
