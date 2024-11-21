4bbe00f58c255d4f91adc9d476e112ae0d01551b
Mathias Boudreau <mabo1123@ulaval.ca>
2024-11-21 03:19:08 -0500

    Création des fichiers (views) qui vont contenir les futurs pages

---
+++ frontend/src/views/Flight.vue
@@ -0,0 +1,19 @@
+<script setup>
+import MyMap from "../components/MyMap.vue";
+import MyChart from "../components/MyChart.vue";
+import MyStatus from "../components/MyStatus.vue";
+import MyThreeView from "../components/MyThreeView.vue";
+import MyOther from "../components/MyOther.vue";
+import MyConsole from "../components/MyConsole.vue";
+</script>
+
+<template>
+  <div class="grid">
+    <MyMap></MyMap>
+    <MyChart></MyChart>
+    <MyStatus></MyStatus>
+    <MyThreeView></MyThreeView>
+    <MyOther></MyOther>
+    <MyConsole></MyConsole>
+  </div>
+</template>
